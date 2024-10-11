6f815371e2bc6463c270244952bca3bb4f19029e
heo-gom <gjtjdwo1004@naver.com>
2024-10-11 16:45:58 +0900

    로그인 방식 변경

--- lib/screens/home/home_sceen.dart
+++ lib/screens/home/home_sceen.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fronttodayhome/screens/home/component/home_login.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, "/login");
-              },
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      borderRadius: BorderRadius.circular(7)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(14.0),
-                    child: Text("로그인하기",
-                        style: TextStyle(color: Colors.white)),
-                  )),
-            )),
-      ],
-    );
+     return ListView(
+       children: [
+         HomeLogin(),
+       ],
+     );
   }
 }
