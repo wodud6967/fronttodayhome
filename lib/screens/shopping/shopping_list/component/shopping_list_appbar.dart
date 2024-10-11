7d9332e2cee50354ad49c06645cb664cf4fbb448
unikyul <zldzkdbs97@gmail.com>
2024-10-11 17:00:52 +0900

    feat 리뷰 미완성(상세보기)

--- lib/screens/shopping/shopping_list/component/shopping_list_appbar.dart
+++ lib/screens/shopping/shopping_list/component/shopping_list_appbar.dart
@@ -8,7 +8,8 @@ class ShoppingListAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12))),
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black12))),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
         child: SizedBox(
@@ -16,17 +17,50 @@ class ShoppingListAppbar extends StatelessWidget {
           height: 45,
           child: Row(
             children: [
-              InkWell(onTap:(){ Navigator.pop(context);},child: Icon(Icons.arrow_back,size: 25,)),
+              InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 25,
+                  )),
               const SizedBox(width: 8),
-              InkWell(onTap:(){ Navigator.pop(context);},child: Icon(Icons.home_outlined,size: 25,)),
+              InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.home_outlined,
+                    size: 25,
+                  )),
               const Spacer(),
-              Center(child: Text("$name", style: TextStyle(fontSize: 16,color: Colors.black,fontWeight: FontWeight.w600),)),
+              Center(
+                  child: Text(
+                "$name",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              )),
               const Spacer(),
-              InkWell(onTap:(){ Navigator.pop(context);},child: Icon(Icons.search_rounded,size: 25,)),
+              InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.search_rounded,
+                    size: 25,
+                  )),
               const SizedBox(width: 8),
-              InkWell(onTap:(){ Navigator.pushNamed(context, "/cart");},child: Icon(Icons.shopping_cart_outlined,size: 25,)),
-
-
+              InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, "/cart");
+                  },
+                  child: Icon(
+                    Icons.shopping_cart_outlined,
+                    size: 25,
+                  )),
             ],
           ),
         ),
