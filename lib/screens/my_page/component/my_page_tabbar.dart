68d5e54f3df6fab4bc2c072c6927ecbd04379e09
unikyul <zldzkdbs97@gmail.com>
2024-10-14 15:21:13 +0900

    주문 목록page

--- lib/screens/my_page/component/my_page_tabbar.dart
+++ lib/screens/my_page/component/my_page_tabbar.dart
@@ -45,25 +45,128 @@ class _TabBarView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TabBarView(
-      controller: _tabController,
+      controller: _tabController, // 탭바 컨트롤러
       children: [
         Padding(
           padding: const EdgeInsets.all(8.0),
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisSpacing: 10,
-              crossAxisCount: 3,
-              mainAxisSpacing: 10,
-            ),
-            itemCount: 42,
+          child: ListView.builder(
+            itemCount: 10, // 상품 갯수
             itemBuilder: (context, index) {
-              return Image.network(
-                "https://picsum.photos/id/${index + 1}/500/500",
+              return Card(
+                elevation: 4, // 그림자
+                margin: EdgeInsets.symmetric(vertical: 16),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // 배송 정보
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '배송완료',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green,
+                            ),
+                          ),
+                          Text('10/1(일) 도착'),
+                        ],
+                      ),
+                      Divider(),
+                      // 상품 정보
+                      Row(
+                        children: [
+                          // 상품 이미지
+                          Image.network(
+                            "https://picsum.photos/id/${index + 1}/100/100",
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(width: 10),
+                          // 상품명 및 가격 정보
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '제품명 $index',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                SizedBox(height: 5),
+                                Text('495,000원  •  1개'),
+                              ],
+                            ),
+                          ),
+                          // 장바구니 담기 버튼
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '상품 정보',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: TextButton.styleFrom(
+                              minimumSize: Size(100, 40),
+                              side: BorderSide(color: Colors.blue),
+                              backgroundColor: Colors.lightBlue,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      // 교환 및 배송조회 버튼
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              print("교환, 반품 신청");
+                            },
+                            child: Text(
+                              '교환, 반품 신청',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(150, 40),
+                              backgroundColor: Colors.lightBlue,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '리뷰쓰기',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: TextButton.styleFrom(
+                              minimumSize: Size(100, 40),
+                              backgroundColor: Colors.lightBlue,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               );
             },
           ),
         ),
-        Container(color: Colors.red),
+        Container(color: Colors.red), // 다른 탭의 내용
       ],
     );
   }
@@ -85,7 +188,7 @@ class _Tabbar extends StatelessWidget {
       unselectedLabelColor: Colors.grey,
       indicatorColor: Colors.blue,
       tabs: const [
-        Tab(text: "콘텐츠"), // 첫 번째 탭
+        Tab(text: "구매 제품"), // 첫 번째 탭
         Tab(text: "스크랩"), // 두 번째 탭
       ],
     );
