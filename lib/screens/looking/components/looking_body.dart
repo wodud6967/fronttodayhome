94a75c7cbf5177297093da99ab5128f78cc7af0b
wodud6967 <wodud6967@gmail.com>
2024-10-16 12:07:56 +0900

    fect: lookg 미완료

--- lib/screens/looking/components/looking_body.dart
+++ lib/screens/looking/components/looking_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fronttodayhome/components/image_container.dart';
 import 'package:fronttodayhome/models/neighborhood_life.dart';
+import 'package:fronttodayhome/theme.dart';
 
 class LookingBody extends StatelessWidget {
   final NeighborhoodLife neighborhoodLife;
@@ -47,13 +49,18 @@ class LookingBody extends StatelessWidget {
           Container(
             padding: EdgeInsets.all(4),
             decoration: BoxDecoration(
-              shape: BoxShape.rectangle
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+                color: Color.fromRGBO(247, 247, 247, 1)),
+            child: Text(
+              neighborhoodLife.category,
+              style: TextTheme().bodyMedium,
             ),
           ),
           Text(
             neighborhoodLife.date,
+            style: TextTheme().bodyMedium, //작동안함
             //스타일해야될듯
-
           ),
         ],
       ),
@@ -64,9 +71,28 @@ class LookingBody extends StatelessWidget {
   Widget _buildWriter() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Container(
-        color: Colors.red[100],
-        height: 50,
+      child: Row(
+        children: [
+          ImageContainer(
+            width: 30,
+            height: 30,
+            borderRadius: 15,
+            imageUrl: neighborhoodLife.profileImgUri,
+          ),
+          const SizedBox(width: 8.0),
+          Text.rich(
+            TextSpan(children: [
+              TextSpan(
+                text: '${neighborhoodLife.userName}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+            ]),
+          ),
+        ],
       ),
     );
   }
@@ -75,20 +101,31 @@ class LookingBody extends StatelessWidget {
   Widget _buildWriting() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      child: Container(
-        color: Colors.blue,
-        height: 50,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          neighborhoodLife.content,
+          style: textTheme().bodyLarge,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.start,
+        ),
       ),
     );
   }
 
   // 기존의 _buildImage 위젯을 다시 통합
   Widget _buildImage() {
-    return Padding(
-      padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-      child: Container(
-        color: Colors.black,
-        height: 150,
+    return Visibility(
+      visible: neighborhoodLife.contentImgUri != 's',//이부분 나중에 공백으로
+      child: Padding(
+        padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        child: Image.network(
+          'https://picsum.photos/id/872/200/300?grayscale',
+          height: 200,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
